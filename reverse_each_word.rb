459ff67fd9a0495176cf2f9d45e4d9a5bab29d17
Michael Kim <m.kim4247@gmail.com>
2020-10-08 01:43:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
   sentence_arr = sentence.split
-  sentence_arr.collect do |word|
+  new_sentence = sentence_arr.collect do |word|
     new_sentence = word.reverse
   end
+  new_sentence.join
 end
